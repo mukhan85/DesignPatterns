46cf71a7a84e5c40e2a7bc4f909fb36eb59f930f
mukhan85 <mukhan.m@gmail.com>
2015-10-16 23:30:39 +0100

    Converted into Maven Project.

---
+++ src/main/java/kz/designpatterns/TestMain.java
@@ -0,0 +1,9 @@
+package kz.designpatterns;
+/**
+ * Created by mmyrzaku on 16/10/2015.
+ */
+public class TestMain {
+    public static void main(String ... args) {
+        System.out.println("Test");
+    }
+}
